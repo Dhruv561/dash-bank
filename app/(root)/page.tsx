06018bdd34df1ee7b973d5505977221799883698
Dhruv Verma <104663445+Dhruv561@users.noreply.github.com>
2025-08-05 12:17:04 +1000

    implement home balance chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+  const loggedIn = { firstName: "Dhruv" };
   return (
     <section className="home">
       <div className="home-content">
-        <header className="home-header">Hello There</header>
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account transactions efficiently."
+        />
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
       </div>
     </section>
   );
