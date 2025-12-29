bb57e946c83c0a8b1d1a80293812c44fa9278561
Dhruv Verma <104663445+Dhruv561@users.noreply.github.com>
2025-12-29 14:26:02 +1100

    implemented sign-in form

---
+++ components/CustomInput.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { authFormSchema } from "@/lib/utils";
+
+interface CustomInput {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name: FieldPath<z.infer<typeof authFormSchema>>;
+  label: string;
+  placeholder: string;
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={name === "password" ? "password" : "text"}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage className="form-message mt-2" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
